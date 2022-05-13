c324159fc2e906b9e90b080ffb82d39347231688
AutumnzLing <2353727288@qq.com>
2022-05-13 23:22:49 +0800

    优化update代码，依据梨子的意见做出部分修改

--- src/plugins/DicePP/module/misc/update_command.py
+++ src/plugins/DicePP/module/misc/update_command.py
@@ -1,5 +1,5 @@
 import os
-from typing import List, Tuple, Any, Literal
+from typing import List, Tuple, Any, Literal, Callable, Optional
 
 from core.bot import Bot
 from core.command.const import *
@@ -9,6 +9,7 @@ from core.communication import MessageMetaData, PrivateMessagePort, GroupMessage
 from core.config import PROJECT_PATH
 from utils.dicegit import GitRepository
 
+BOT_ROOT_PATH = os.path.dirname(os.path.dirname(os.path.dirname(PROJECT_PATH)))
 CFG_UPDATE_SOURCE = "update_source"
 CFG_DAILY_CHECK = "update_daily_check"
 LOC_UPDATE = "update"
@@ -23,13 +24,13 @@ class UpdateCommand(UserCommandBase):
 
     def __init__(self, bot: Bot):
         super().__init__(bot)
-        bot.loc_helper.register_loc_text(LOC_UPDATE, "{TextArea}")  # ToDo: 没必要在这里设置自定义文本, 直接删掉吧
-
         bot.cfg_helper.register_config(CFG_UPDATE_SOURCE, "gitee", "可自定义的更新源, 目前支持gitee(推荐国内使用)与github.")
-        bot.cfg_helper.register_config(CFG_DAILY_CHECK, "0", "每日更新检查, 1为检查, 0为不检查, 可以通过指令设置.")
+        bot.cfg_helper.register_config(CFG_DAILY_CHECK, "0", "每日更新检查, 1为检查, 0为不检查.")
 
         self.update_source: Literal["gitee", "github"] = "gitee"
         self.daily_check: bool = False
+        self.git_repo = None
+        self.master_list = []
 
     def delay_init(self) -> List[str]:
         try:
@@ -42,13 +43,16 @@ class UpdateCommand(UserCommandBase):
             self.daily_check = (daily_check_val != 0)
         except (ValueError, IndexError):
             self.daily_check = False
+        try:
+            self.master_list = self.bot.get_master_ids()
+        finally:
+            pass
         return []
 
     def can_process_msg(self, msg_str: str, meta: MessageMetaData) -> Tuple[bool, bool, Any]:
-        master_list = self.bot.get_master_ids()
         should_proc: bool = False
         should_pass: bool = False
-        if meta.user_id not in master_list:
+        if meta.user_id not in self.master_list[0]:
             return should_proc, should_pass, None
 
         should_proc: bool = msg_str.startswith(".update")
@@ -67,46 +71,49 @@ class UpdateCommand(UserCommandBase):
             feedback = "初始化Git仓库失败, 请检查您的配置项是否正确。"
             return [BotSendMsgCommand(self.bot.account, feedback, [port])]
         try:
-            git_repo = GitRepository(PROJECT_PATH, git_path, self.update_source)
+            self.git_repo = GitRepository(BOT_ROOT_PATH, git_path, self.update_source)
         except AssertionError:
-            raise NotImplementedError()  # ToDo
+            raise NotImplementedError("未能成功初始化git仓库。")
 
         if arg_str == "更新":
-            textarea = git_repo.update()
-            feedback: str = self.format_loc(LOC_UPDATE, TextArea=textarea)
+            feedback: str = self.bot.register_task(self.get_update)
         elif arg_str == "初始化":
-            textarea = git_repo.refresh()
-            feedback: str = self.format_loc(LOC_UPDATE, TextArea=textarea)
+            feedback: str = self.bot.register_task(self.refresh)
         elif not arg_str:
-            textarea = git_repo.get_update()
-            feedback: str = self.format_loc(LOC_UPDATE, TextArea=textarea)
+            feedback: str = self.bot.register_task(self.update)
         else:
             feedback = "请输入.help update查看指令说明"
         return [BotSendMsgCommand(self.bot.account, feedback, [port])]
 
+    def get_update(self) -> List[BotSendMsgCommand]:
+        return [BotSendMsgCommand(self.bot.account, self.git_repo.get_update(), [PrivateMessagePort(self.master_list[0])])]
+
+    def update(self):
+        return [BotSendMsgCommand(self.bot.account, self.git_repo.update(), [PrivateMessagePort(self.master_list[0])])]
+
+    def refresh(self):
+        return [BotSendMsgCommand(self.bot.account, self.git_repo.refresh(), [PrivateMessagePort(self.master_list[0])])]
+
     def tick_daily(self) -> List[BotCommandBase]:
+        if self.bot.cfg_helper.get_config(CFG_DAILY_CHECK) == 0:
+            return []
         if self.update_source.lower() == "github":
             git_path = dpp_github_path
         elif self.update_source.lower() == "gitee":
             git_path = dpp_gitee_path
         else:
-            self.bot.send_msg_to_master("初始化Git仓库失败, 请检查您的配置项是否正确。")  # ToDo: 错误用法
-            return []
+            return [BotSendMsgCommand(self.bot.account, "初始化Git仓库失败, 请检查您的配置项是否正确。", [PrivateMessagePort(self.master_list[0])])]
         try:
-            gitrepo = GitRepository(PROJECT_PATH, git_path, self.update_source)
+            gitrepo = GitRepository(BOT_ROOT_PATH, git_path, self.update_source)
         except AssertionError:
-            raise NotImplementedError()  # ToDo: 请处理异常
-        textarea = gitrepo.get_update()
-        textarea1 = gitrepo.is_dirty_check()
-        feedback: str = self.format_loc(LOC_UPDATE, TextArea=textarea)
-        feedback1: str = self.format_loc(LOC_UPDATE, TextArea=textarea1)
-        if not textarea1:
-            self.bot.send_msg_to_master(feedback)  # ToDo: 错误用法
-            return []
+            raise NotImplementedError("未能成功初始化git仓库。")
+        feedback: str = gitrepo.get_update()
+        feedback1: str = gitrepo.is_dirty_check()
+        if not feedback1:
+            return [BotSendMsgCommand(self.bot.account, feedback, [PrivateMessagePort(self.master_list[0])])]
         else:
-            self.bot.send_msg_to_master(feedback)  # ToDo: 错误用法
-            self.bot.send_msg_to_master(feedback1)  # ToDo: 错误用法
-        return []
+            return [BotSendMsgCommand(self.bot.account, feedback, [PrivateMessagePort(self.master_list[0])]),
+                    BotSendMsgCommand(self.bot.account, feedback1, [PrivateMessagePort(self.master_list[0])])]
 
     def get_help(self, keyword: str, meta: MessageMetaData) -> str:
         if keyword == "update":  # help后的接着的内容
--- src/plugins/DicePP/utils/dicegit.py
+++ src/plugins/DicePP/utils/dicegit.py
@@ -1,6 +1,5 @@
-from git import InvalidGitRepositoryError, NoSuchPathError, GitCommandError
-
-import utils
+from typing import Union
+from git import InvalidGitRepositoryError, NoSuchPathError, GitCommandError, Repo
 from utils.logger import dice_log
 
 IS_NEWEST = False
@@ -10,17 +9,13 @@ def is_network_error(err_str: str) -> bool:
     return "errno 10054" in err_str or "Timed out" in err_str or "Could not resolve host" in err_str
 
 
-class GitRepository(object):
+class GitRepository:
     """
     git仓库管理
     """
 
     def __init__(self, local_path, repo_url, update_source):
         try:
-            import nonebot  # ToDo 只列出和本功能相关的库
-            import openpyxl
-            import rsa
-            import yaml
             import os
             import git
         except ImportError as e:
@@ -41,82 +36,50 @@ class GitRepository(object):
 
     def is_dirty_check(self) -> str:
         if self.repo.is_dirty() and self.repo.head.name == "HEAD" and self.repo.remote().name == "origin":
-            return "检测到DicePP源码被修改。若需清除该修改，请键入 .update 初始化 以清除其它更改。"
+            return "检测到DicePP源码被修改.若需清除该修改,请键入 .update 初始化 以清除其它更改."
         else:
             return ""
 
-    def update(self):  # ToDo 返回类型不明确
-        global IS_NEWEST
-        if IS_NEWEST:  # ToDo 这里需要更新IS_NEWEST, 否则用户一直用更新指令的话永远提示是最新的
-            return "已是最新。"
+    def update(self) -> str:
+        marking = self.get_update()
+        if marking == "已是最新. ":
+            return "已是最新."
         try:
-            master = self.repo.heads.master
-        except Exception as e:
-            return e  # ToDo 不要直接返回异常, 很奇怪, 至少在这里转成字符串吧
-        try:
-            other = self.repo.create_head("other", "head")
-        except Exception as e:
-            return e
-        try:
-            other.checkout()
-        except git.exc.CheckoutError as e:
-            return e
-        try:
-            self.repo.remote().fetch()
+            self.repo.git.fetch(self.repo_url + " " + "refs/heads/master:refs/heads/origin/master")
         except ValueError as e:
-            return e
+            return str(e)
         except GitCommandError as e:
             if e.stderr and is_network_error(e.stderr):
                 return "网络连接异常, 请确认没有开启VPN或代理服务, 并再次重试"
-            return e
+            return str(e)
         try:
-            master.checkout()
-        except git.exc.CheckoutError as e:
-            return e
-        try:
-            self.repo.index.merge_tree(other)  # ToDo 这里提示类型不正确, 检查一下
+            self.repo.git.merge("origin/master")
         except GitCommandError as e:
-            return e
-        try:
-            other.delete(self.repo, other)
-        except Exception as e:
-            utils.logger.dice_log(e)
-            return "更新失败：", e
-        return "更新完成，请输入.m reboot重启bot以应用更新。"
+            return str(e)
+        return "更新完成,请输入.m reboot重启bot以应用更新."
 
-    def get_update(self):  # ToDo 返回类型不明确
-        global IS_NEWEST  # ToDo 尽量不要用全局变量
+    def get_update(self) -> str:
         try:
-            c = self.repo.git.log("master..origin/master", "-1", "--pretty={format:%H,%s}")
+            c = self.repo.git.log("master..origin/master", "-1", "--pretty={hash:%H,%s}")
         except Exception as e:
-            return "检查更新失败。原因:", e  # ToDo 所有符号用半角, 不要用。和， 而是用.和,
+            return "检查更新失败. 原因: \n" + str(e)
         if c:
-            IS_NEWEST = False
-            return "检测到更新，内容如下", c
-        IS_NEWEST = True
-        return "已是最新。"
+            return "检测到更新, 内容如下: \n" + str(c)
+        return "已是最新. "
 
-    def refresh(self):  # ToDo 返回类型不明确
+    def refresh(self) -> str:
         try:
-            self.repo.git.refresh()  # ToDo 这一步和仓库有关系吗? 确认一下
+            self.repo.git.refresh()
         except Exception as e:
-            return "初始化代码失败：", e
-        return "已成功初始化DicePP代码。"
+            return "初始化代码失败：" + str(e)
+        return "已成功初始化DicePP代码."
 
-    def get_git_repo(self):  # ToDo 返回类型不明确
+    def get_git_repo(self) -> Union[Repo, str]:
         try:
-            git_repo = git.Repo(self.local_path)
+            git_repo = Repo(self.local_path)
         except InvalidGitRepositoryError as e:
-            return "git仓库初始化失败", e
+            return "git仓库初始化失败, 原因如下: \n" + str(e)
         except NoSuchPathError as e:
-            return "git仓库初始化失败", e
+            return "git仓库初始化失败, 原因如下: \n" + str(e)
         return git_repo
 
-
-if __name__ == "__main__":
-    import os
-    import git
-
-    test = GitRepository(r"D:/tmp/dicepp", "https://gitee.com/pear_studio/nonebot-dicepp.git", "gitee")
-    test.get_update()
-    test.update()
